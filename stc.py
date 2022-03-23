30caa881bedb92991bcfe78515b863b471c1ba91
Cian <bagcais@gmail.com>
2022-03-23 16:22:26 +0000

    School Tour Leetcode Matching Parentheses

--- stc.py
+++ stc.py
@@ -1,1 +1,22 @@
-test
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        if s[0] not in ["{","[","("]:
+            return False
+        for i in range(len(s)):
+            if s[i] in ["{","[","("]:
+                stack.append(s[i])
+        if s[i] in ["}","]",")"]:
+            if s[i] == "}":
+                if stack[-1] == "{":
+                    stack.pop()
+            if s[i] == "]":
+                if stack[-1] == "[":
+                    stack.pop()
+            if s[i] == ")":
+                if stack[-1] == "(":
+                    stack.pop()
+            if stack == []:
+                 return True
+            else:
+                 return False
